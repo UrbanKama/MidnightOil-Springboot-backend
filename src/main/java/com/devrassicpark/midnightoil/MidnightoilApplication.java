034fb284d27b6f71be3f2abe4e0ab7c5a1a67d7a
joseph kamau <joseph.kamau@mastek.com>
2023-09-03 00:18:22 +0100

    initial project files commit

---
+++ src/main/java/com/devrassicpark/midnightoil/MidnightoilApplication.java
@@ -0,0 +1,13 @@
+package com.devrassicpark.midnightoil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidnightoilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidnightoilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devrassicpark/midnightoil/MidnightoilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrassicpark.midnightoil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidnightoilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
